e0ba9cbdb68fc05987846f0e48aefe6e2592fa07
Muhammad hashim <hashimgujjar4447@gmail.com>
2026-03-28 21:52:54 +0500

    Expose user id in UserSerializer
    Add the Account primary key ('id') to UserSerializer.fields so API responses include the user id. This enables clients to reference users by id for linking and subsequent requests.

--- api/serializers/common_serializers.py
+++ api/serializers/common_serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ['first_name', 'last_name', 'email', 'username', 'date_joined']
+        fields = ['first_name', 'last_name', 'email', 'username', 'date_joined','id']
